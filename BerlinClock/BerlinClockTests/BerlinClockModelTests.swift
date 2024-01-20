68a5d3f514e79cf40e038a818d63780ddc75b3fb
dev-2024 <test@gmail.com>
2024-01-20 12:22:59 +0530

    refactor: Improve readability and consistency in BerlinClockModelTests

--- BerlinClock/BerlinClockTests/BerlinClockModelTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockModelTests.swift
@@ -4,15 +4,25 @@ import XCTest
 
 class BerlinClockModelTests: XCTestCase {
   
+  var model: BerlinClockModel!
+  
+  override func setUp() {
+    super.setUp()
+    model = BerlinClockModel()
+  }
+  
+  override func tearDown() {
+    model = nil
+    super.tearDown()
+  }
+  
   func testUpdateSecondsLamps() {
-    var model = BerlinClockModel()
     model.updateSecondsLamps(for: 12)
     XCTAssertTrue(model.secondsLamps[0].isOn)
     XCTAssertEqual(model.secondsLamps[0].color, .yellow)
   }
   
   func testUpdateUppperHoursHoursLamps() {
-    var model = BerlinClockModel()
     model.updateUppperRowHoursLamps(for: 5)
     XCTAssertEqual(model.upperHoursLamps, [Lamp(color: .red, isOn: true), Lamp(color: .gray, isOn: false), Lamp(color: .gray, isOn: false), Lamp(color: .gray, isOn: false)])
     
