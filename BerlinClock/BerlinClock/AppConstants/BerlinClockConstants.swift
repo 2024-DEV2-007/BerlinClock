6328c708a8ff3ef4fc90837bae90f6e0ef433219
dev-2024 <test@gmail.com>
2024-01-20 12:45:00 +0530

    refactor : Add BerlinClockConstants for lamp row count and Improve organization and readability in BerlinClockViewModel

---
+++ BerlinClock/BerlinClock/AppConstants/BerlinClockConstants.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+// Berlin Clock Rows
+struct BerlinClockConstants {
+  static let secondsLampsCount = 1
+  static let upperRowHoursLampsCount = 4
+  static let lowerRowHoursLampsCount = 4
+  static let upperRowMinutesLampsCount = 11
+  static let lowerRowMinutesLampsCount = 4
+}
+
--- BerlinClock/BerlinClock/Model/BerlinClockModel.swift
+++ BerlinClock/BerlinClock/Model/BerlinClockModel.swift
@@ -3,9 +3,12 @@ import Foundation
 
 struct BerlinClockModel: Equatable {
   var secondsLamps: [Lamp] = [Lamp(color: .gray, isOn: false)]
-  var upperHoursLamps: [Lamp] = Array(repeating: Lamp(color: .gray, isOn: false), count: 4)
-  var lowerHoursLamps: [Lamp] = Array(repeating: Lamp(color: .gray, isOn: false), count: 4)
-  
+  var upperHoursLamps: [Lamp] = Array(repeating: Lamp(color: .gray, isOn: false), count: BerlinClockConstants.upperRowHoursLampsCount)
+  var lowerHoursLamps: [Lamp] = Array(repeating: Lamp(color: .gray, isOn: false), count: BerlinClockConstants.lowerRowHoursLampsCount)
+}
+
+extension BerlinClockModel {
+    
   mutating func updateSecondsLamps(for seconds: Int) {
     let isOn = seconds % 2 == 0
     let color: LampColor = isOn ? .yellow : .gray
@@ -13,8 +16,8 @@ struct BerlinClockModel: Equatable {
   }
   
   mutating func updateUppperRowHoursLamps(for hours: Int) {
-    let topHours = (min(4, hours / 5))
-    upperHoursLamps = Array(repeating: Lamp(color: .red, isOn: true), count: topHours) + Array(repeating: Lamp(color: .gray, isOn: false), count: max(0, 4 - topHours))
+    let upperHours = (min(4, hours / 5))
+    upperHoursLamps = Array(repeating: Lamp(color: .red, isOn: true), count: upperHours) + Array(repeating: Lamp(color: .gray, isOn: false), count: max(0, 4 - upperHours))
   }
   
   mutating func updateLowerRowHoursLamps(for hours: Int) {
