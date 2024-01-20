03014308044c7b9c13c53fbcf0622a0c86c476a1
dev-2024 <test@gmail.com>
2024-01-20 12:30:21 +0530

    Test : Add BerlinClockModel test for update lower row hours lamps.

--- BerlinClock/BerlinClock/Model/BerlinClockModel.swift
+++ BerlinClock/BerlinClock/Model/BerlinClockModel.swift
@@ -4,6 +4,7 @@ import Foundation
 struct BerlinClockModel: Equatable {
   var secondsLamps: [Lamp] = [Lamp(color: .gray, isOn: false)]
   var upperHoursLamps: [Lamp] = Array(repeating: Lamp(color: .gray, isOn: false), count: 4)
+  var lowerHoursLamps: [Lamp] = Array(repeating: Lamp(color: .gray, isOn: false), count: 4)
   
   mutating func updateSecondsLamps(for seconds: Int) {
     let isOn = seconds % 2 == 0
@@ -14,5 +15,11 @@ struct BerlinClockModel: Equatable {
   mutating func updateUppperRowHoursLamps(for hours: Int) {
     let topHours = (min(4, hours / 5))
     upperHoursLamps = Array(repeating: Lamp(color: .red, isOn: true), count: topHours) + Array(repeating: Lamp(color: .gray, isOn: false), count: max(0, 4 - topHours))
+  }
+  
+  mutating func updateLowerRowHoursLamps(for hours: Int) {
+    // Calculate the remainder when dividing hours by 5 to get the bottom hours
+    let lowerHours = hours % 5
+    lowerHoursLamps = Array(repeating: Lamp(color: .red, isOn: true), count: lowerHours) + Array(repeating: Lamp(color: .gray, isOn: false), count: max(0, 4 - lowerHours))
   }
 }
--- BerlinClock/BerlinClockTests/BerlinClockModelTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockModelTests.swift
@@ -29,4 +29,12 @@ class BerlinClockModelTests: XCTestCase {
     model.updateUppperRowHoursLamps(for: 23)
     XCTAssertEqual(model.upperHoursLamps, [Lamp(color: .red, isOn: true), Lamp(color: .red, isOn: true), Lamp(color: .red, isOn: true), Lamp(color: .red, isOn: true)])
   }
+  
+  func testUpdateLowerHoursLamps() {
+    model.updateLowerRowHoursLamps(for: 5)
+    XCTAssertEqual(model.lowerHoursLamps, [Lamp(color: .gray, isOn: false), Lamp(color: .gray, isOn: false), Lamp(color: .gray, isOn: false), Lamp(color: .gray, isOn: false)])
+    
+    model.updateLowerRowHoursLamps(for: 16)
+    XCTAssertEqual(model.lowerHoursLamps, [Lamp(color: .red, isOn: true), Lamp(color: .gray, isOn: false), Lamp(color: .gray, isOn: false), Lamp(color: .gray, isOn: false)])
+  }
 }
