6ea5d72c5b5cce89242b099a8bc96284df9b424c
dev-2024 <test@gmail.com>
2024-01-21 14:40:28 +0530

    refactor: Add date helper class for get date components

---
+++ BerlinClock/BerlinClock/Helper/DateHelper.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct DateHelper {
+  static func getCurrentDate() -> Date {
+    return Date()
+  }
+  
+  static func getComponents(from date: Date) -> DateComponents {
+    return Calendar.current.dateComponents([.hour, .minute, .second], from: date)
+  }
+}
