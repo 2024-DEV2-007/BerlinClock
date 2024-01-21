2c8062d1efab68ca4437c02f58f7d2c2065eae4a
dev-2024 <test@gmail.com>
2024-01-21 14:27:45 +0530

    test: Add test for BerlinClockViewModel updating lamps with timer

--- BerlinClock/BerlinClockTests/BerlinViewModelTest.swift
+++ BerlinClock/BerlinClockTests/BerlinViewModelTest.swift
@@ -10,4 +10,19 @@ class BerlinViewModelTest: XCTestCase {
     XCTAssertNotNil(viewModel)
     XCTAssertNotNil(viewModel.berlinClockModel)
   }
+  
+  func testViewModelUpdatesLamps() {
+    let viewModel = BerlinClockViewModel()
+    let initialLampsState = viewModel.berlinClockModel
+    
+    viewModel.startClock()
+    
+    let expectation = XCTestExpectation(description: "Wait for timer to trigger")
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 5)
+    
+    XCTAssertNotEqual(viewModel.berlinClockModel, initialLampsState)
+  }
 }
